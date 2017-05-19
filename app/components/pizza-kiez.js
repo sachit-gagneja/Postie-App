83505a92d829d1e234ca7103d7126c18130aa8e5
lislis <mail@lislis.de>
2017-05-19 15:37:04 +0200

    pizza time

--- app/components/pizza-kiez.js
+++ app/components/pizza-kiez.js
@@ -3,8 +3,15 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'section',
   classNames: ['pizza-slice'],
+  classNameBindings: ['isShowingPlaces:stop:run'],
+
+  isShowingPlaces: false,
 
   filteredPlaces: Ember.computed.filter('places', function (place, index, array) {
     return place.kiez === this.get('kiez.name');
-  })
+  }),
+
+  click() {
+    return this.toggleProperty('isShowingPlaces');
+  }
 });
